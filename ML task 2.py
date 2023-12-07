ddef4f9c6cff1f0c080b93dbef22b59a61646f14
LearningGB <125333683+LearningGB@users.noreply.github.com>
2023-12-07 12:58:24 +0800

    Update ML task 2.py

--- ML task 2.py
+++ ML task 2.py
@@ -1,151 +1,129 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score
-from sklearn.preprocessing import StandardScaler
-import pickle
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 class ScratchLogisticRegression():
-    def __init__(self, num_iter=1000, lr=0.01, bias=True, lambda_reg=0.1, verbose=False):
+    def __init__(self, num_iter, lr, bias, verbose, reg_param=0.1):
         self.iter = num_iter
         self.lr = lr
         self.bias = bias
-        self.lambda_reg = lambda_reg
         self.verbose = verbose
+        self.reg_param = reg_param
         self.loss = np.zeros(self.iter)
         self.val_loss = np.zeros(self.iter)
-        
+        self.coef_ = None
+
     def _sigmoid(self, z):
         return 1 / (1 + np.exp(-z))
-    
+
     def _gradient_descent(self, X, error):
-        regularization_term = (self.lambda_reg / len(X)) * self.theta[1:]
-        self.theta -= self.lr * (np.dot(X.T, error) + regularization_term)
-    
-    def _calculate_loss(self, y_true, y_pred):
-        regularization_term = (self.lambda_reg / (2 * len(y_true))) * np.sum(self.theta[1:]**2)
-        loss = -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred)) + regularization_term
-        return loss
-    
+        gradient = np.dot(X.T, error) / len(X)
+        self.coef_ -= self.lr * gradient
+
+    def _compute_regularization_term(self):
+        return (self.reg_param / (2 * len(self.coef_))) * np.sum(self.coef_[1:]**2)
+
+    def _compute_loss(self, h, y):
+        regularization_term = self._compute_regularization_term()
+        return -np.mean(y * np.log(h) + (1 - y) * np.log(1 - h)) + regularization_term
+
     def fit(self, X, y, X_val=None, y_val=None):
-        if self.bias:
-            X = np.column_stack((np.ones(len(X)), X))
-            if X_val is not None:
-                X_val = np.column_stack((np.ones(len(X_val)), X_val))
-        
-        self.theta = np.zeros(X.shape[1])
-        
+        X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+
+        if self.verbose:
+            print("Training...")
+
+        self.coef_ = np.zeros(X_train.shape[1])
+
         for i in range(self.iter):
-            z = np.dot(X, self.theta)
+            z = np.dot(X_train, self.coef_)
             h = self._sigmoid(z)
-            error = h - y
-            self._gradient_descent(X, error)
-            
-            self.loss[i] = self._calculate_loss(y, h)
-            
-            if X_val is not None:
-                z_val = np.dot(X_val, self.theta)
+            error = h - y_train
+
+            self._gradient_descent(X_train, error)
+
+            # Compute training loss
+            self.loss[i] = self._compute_loss(h, y_train)
+
+            # Compute validation loss
+            if X_val is not None and y_val is not None:
+                z_val = np.dot(X_val, self.coef_)
                 h_val = self._sigmoid(z_val)
-                self.val_loss[i] = self._calculate_loss(y_val, h_val)
-            
-            if self.verbose and i % 100 == 0:
-                print(f"Iteration {i}, Training Loss: {self.loss[i]}")
-                
+                self.val_loss[i] = self._compute_loss(h_val, y_val)
+
+            if self.verbose and (i % 100 == 0 or i == self.iter - 1):
+                print(f"Iteration {i}: Training Loss = {self.loss[i]:.4f}, Validation Loss = {self.val_loss[i]:.4f}")
+
+    def predict(self, X):
+        return np.round(self._sigmoid(np.dot(X, self.coef_)))
+
     def predict_proba(self, X):
-        if self.bias:
-            X = np.column_stack((np.ones(len(X)), X))
-        return self._sigmoid(np.dot(X, self.theta))
-    
-    def predict(self, X, threshold=0.5):
-        return (self.predict_proba(X) >= threshold).astype(int)
+        return self._sigmoid(np.dot(X, self.coef_))
 
-# Problem 5: Learning and Estimation
-iris = load_iris()
-X = iris.data[50:, :2]  # Take only two features for binary classification
-y = (iris.target[50:] == 2).astype(int)  # 1 if virginica, 0 if versicolor
+# Problem 5: Learning and estimation
+# Assuming X_train and y_train are your training data and labels
+X_train = np.random.rand(100, 2)
+y_train = np.random.randint(0, 2, size=100)
 
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+# Create an instance of ScratchLogisticRegression
+lr_scratch = ScratchLogisticRegression(num_iter=1000, lr=0.01, bias=True, verbose=True, reg_param=0.1)
 
-scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_val_scaled = scaler.transform(X_val)
+# Fit the model
+lr_scratch.fit(X_train, y_train)
 
-# Scratch Logistic Regression
-scratch_lr = ScratchLogisticRegression(num_iter=1000, lr=0.01, lambda_reg=0.1, verbose=True)
-scratch_lr.fit(X_train_scaled, y_train, X_val_scaled, y_val)
+# Compare with scikit-learn implementation
+lr_sklearn = LogisticRegression(C=1 / (2 * len(X_train)), max_iter=1000)
+lr_sklearn.fit(X_train, y_train)
 
-# Scikit-learn Logistic Regression
-sklearn_lr = LogisticRegression(C=1 / 0.1, random_state=42)
-sklearn_lr.fit(X_train_scaled, y_train)
+# Print weights
+print("Scratch Coefficients:", lr_scratch.coef_)
+print("Scikit-learn Coefficients:", lr_sklearn.coef_)
 
-# Compare Accuracy, Precision, and Recall
-y_pred_scratch = scratch_lr.predict(X_val_scaled)
-y_pred_sklearn = sklearn_lr.predict(X_val_scaled)
+# Use scikit-learn for evaluation
+X_val = np.random.rand(20, 2)
+y_val = np.random.randint(0, 2, size=20)
 
-print("Scratch Logistic Regression:")
-print(f"Accuracy: {accuracy_score(y_val, y_pred_scratch)}")
-print(f"Precision: {precision_score(y_val, y_pred_scratch)}")
-print(f"Recall: {recall_score(y_val, y_pred_scratch)}")
+y_pred_scratch = lr_scratch.predict(X_val)
+y_pred_sklearn = lr_sklearn.predict(X_val)
 
-print("\nScikit-learn Logistic Regression:")
-print(f"Accuracy: {accuracy_score(y_val, y_pred_sklearn)}")
-print(f"Precision: {precision_score(y_val, y_pred_sklearn)}")
-print(f"Recall: {recall_score(y_val, y_pred_sklearn)}")
+print("Scratch Accuracy:", accuracy_score(y_val, y_pred_scratch))
+print("Scikit-learn Accuracy:", accuracy_score(y_val, y_pred_sklearn))
 
-# Problem 6: Plot of Learning Curve
-plt.plot(scratch_lr.loss, label='Training Loss')
-plt.plot(scratch_lr.val_loss, label='Validation Loss')
-plt.title('Learning Curve')
-plt.xlabel('Iteration')
-plt.ylabel('Loss')
+# Problem 6: Plot of learning curve
+plt.plot(lr_scratch.loss, label="Training Loss")
+plt.plot(lr_scratch.val_loss, label="Validation Loss")
+plt.xlabel("Iterations")
+plt.ylabel("Loss")
 plt.legend()
 plt.show()
 
-# Problem 7: Visualization of Decision Area
-def plot_decision_boundary(X, y, model, title):
-    h = .02  # Step size in the mesh
-    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+# Problem 7: Visualization of decision area
+# Visualization code depends on the number of features in your dataset. If it's 2D, you can plot the decision boundary.
+# For simplicity, let's assume 2D data.
+if X_train.shape[1] == 2:
+    plt.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=plt.cm.Paired)
+    plt.xlabel("Feature 1")
+    plt.ylabel("Feature 2")
+
+    # Plot decision boundary
+    h = .02
+    x_min, x_max = X_train[:, 0].min() - 1, X_train[:, 0].max() + 1
+    y_min, y_max = X_train[:, 1].min() - 1, X_train[:, 1].max() + 1
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
-    Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = lr_scratch.predict(np.c_[xx.ravel(), yy.ravel()])
     Z = Z.reshape(xx.shape)
-    
-    plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.8)
-    plt.scatter(X[:, 0], X[:, 1], c=y, edgecolors='k', cmap=plt.cm.coolwarm)
-    plt.title(title)
-    plt.xlabel('Feature 1')
-    plt.ylabel('Feature 2')
+
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Paired, alpha=0.8)
+    plt.title("Decision Boundary")
     plt.show()
 
-# Plot decision boundary for Scratch Logistic Regression
-plot_decision_boundary(X_val_scaled, y_val, scratch_lr, 'Decision Boundary - Scratch Logistic Regression')
+# Problem 8: (Advance assignment) Saving weights
+# Saving weights using pickle
+import pickle
+with open("weights.pkl", "wb") as f:
+    pickle.dump(lr_scratch.coef_, f)
 
-# Plot decision boundary for Scikit-learn Logistic Regression
-plot_decision_boundary(X_val_scaled, y_val, sklearn_lr, 'Decision Boundary - Scikit-learn Logistic Regression')
-
-# Problem 8: Saving Weights
-def save_weights(model, filename):
-    with open(filename, 'wb') as file:
-        pickle.dump(model.theta, file)
-
-def load_weights(model, filename):
-    with open(filename, 'rb') as file:
-        model.theta = pickle.load(file)
-
-# Save weights of Scratch Logistic Regression
-save_weights(scratch_lr, 'scratch_lr_weights.pkl')
-
-# Load weights into a new instance of Scratch Logistic Regression
-loaded_scratch_lr = ScratchLogisticRegression()
-load_weights(loaded_scratch_lr, 'scratch_lr_weights.pkl')
-
-# Test the loaded model
-y_pred_loaded = loaded_scratch_lr.predict(X_val_scaled)
-
-# Compare Accuracy, Precision, and Recall for the loaded model
-print("\nLoaded Scratch Logistic Regression:")
-print(f"Accuracy: {accuracy_score(y_val, y_pred_loaded)}")
-print(f"Precision: {precision_score(y_val, y_pred_loaded)}")
-print(f"Recall: {recall_score(y_val, y_pred_loaded)}")
+# Saving weights using numpy
+np.savez("weights.npz", coef=lr_scratch.coef_)
